9a988fa470b143646c86a5da57c39bbdb95c74f0
Padilla-11 <pajopaca22@gmail.com>
2025-04-22 22:23:17 -0500

    Update Papeleria.java

--- src/logica/Papeleria.java
+++ src/logica/Papeleria.java
@@ -92,7 +92,7 @@ public class Papeleria {
         String textoBusqueda = texto.toLowerCase();
 
         for (Producto producto : productos) {
-            String codigo = producto.getNombre().toLowerCase();
+            String codigo = producto.getCodigo().toLowerCase();
             String nombre = producto.getNombre().toLowerCase();
             String descripcion = producto.getDescripcion().toLowerCase();
 
